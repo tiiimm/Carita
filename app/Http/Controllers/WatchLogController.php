a059a66558a6db773c86d14cd529a288239ce3d1
tiiimm <51147200+tiiimm@users.noreply.github.com>
2020-01-03 13:08:10 +0800

    added and configured philanthropist and watchlog tables

---
+++ app/Http/Controllers/WatchLogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\WatchLog;
+use Illuminate\Http\Request;
+
+class WatchLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\WatchLog  $watchLog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WatchLog $watchLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\WatchLog  $watchLog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WatchLog $watchLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\WatchLog  $watchLog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WatchLog $watchLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\WatchLog  $watchLog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WatchLog $watchLog)
+    {
+        //
+    }
+}
