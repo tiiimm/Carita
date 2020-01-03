3e99e366ecd9e0bbfe4bdac09f19c5b05f8b226d
tiiimm <51147200+tiiimm@users.noreply.github.com>
2020-01-03 12:37:39 +0800

    configured charities table

--- database/migrations/2020_01_03_042505_create_charities_table.php
+++ database/migrations/2020_01_03_042505_create_charities_table.php
@@ -15,7 +15,20 @@ class CreateCharitiesTable extends Migration
     {
         Schema::create('charities', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('organization');
+            $table->string('contact_number');
+            $table->string('account_name');
+            $table->string('account_number');
+            $table->string('bank');
+            $table->string('address');
+            $table->string('photo');
+            $table->string('description');
+            $table->string('bio_path');
+            $table->string('bio_path_type');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
