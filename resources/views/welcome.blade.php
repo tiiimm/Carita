f328887cbfd42a76fc8394118cc406631d991341
tiiimm <onrubia.fatima98@gmail.com>
2020-05-27 11:35:12 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,9 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-        <meta http-equiv="Access-Control-Allow-Origin" content="*">
+        <!-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> -->
 
-        <title>CaritaApp</title>
+        <title>Carita</title>
 
         <link href="css/app.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
